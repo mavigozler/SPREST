ad114376e139b76b63ddf2d052cd6c055a9c4060
TSMitch <stevepenner1960@gmail.com>
2022-08-13 18:13:44 -0700

    minor changes to main

--- run/src/SPRESTGlobals.d.ts
+++
@@ -1,7 +0,0 @@
-declare enum requestType {
-    TYPE_CURRENT_USER = 0,
-    TYPE_ID = 1,
-    TYPE_LAST_NAME = 2,
-    TYPE_FULL_NAME = 3
-}
-declare const ListItemEntityTypeRE: RegExp, ListFieldEntityTypeRE: RegExp, ListEntityTypeRE: RegExp, ContentTypeEntityTypeRE: RegExp, ViewEntityTypeRE: RegExp, emailAddressRegex: RegExp;
